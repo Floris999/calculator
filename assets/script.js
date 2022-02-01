03c1ba1a109c4ed2a0a902b4f8f2d74e93a05d91
Floris <floris__2@hotmail.com>
2022-02-01 16:30:33 +0100

    selectName function fix

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,31 @@
-import {verhuurderInfo} from "./data";
+// start of "database"
+let verhuurderInfo = {
+    _verhuurder: [
+      "Ad-Rem",
+      "Bo-Rent",
+      "Hertz"
+    ],
+    _bus: [
+      "Citroën Berlingo",
+      "Volkswagen Crafter",
+      "Toyota Proace",
+      "MB Sprinter"
+    ],
+    _ppd: [
+      17.50,
+      34.95,
+      47.50,
+      51.00
+    ],
+    _ppk: [
+      0.15,
+      0.23,
+      0.18,
+      0.15
+    ]
+  };
+  // End of "database"
+
 // Adrem data
 let adremName = verhuurderInfo._verhuurder[0];
 let adremBus = verhuurderInfo._bus[0];
